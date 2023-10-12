7eeb31c1b955bfac46363870168991cdd016e20f
Oskar <o.skoglund09@gmail.com>
2023-10-12 20:42:04 +0200

    added PheromoneGrid

--- Assets/Scripts/Ant.cs
+++ Assets/Scripts/Ant.cs
@@ -1,4 +1,3 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Ant : MonoBehaviour
@@ -16,6 +15,7 @@ public class Ant : MonoBehaviour
     public float viewAngle = 120f;
 
     public LayerMask foodLayer;
+    public LayerMask nestLayer;
     private bool hasFood = false;
     private bool hasFoodInSight = false;
 
@@ -26,18 +26,23 @@ public class Ant : MonoBehaviour
         {
             // If the ant has no food, it will wander around.
             Wander();
+        }
+        else if (!hasFood && hasFoodInSight)
+        {
+            // If the ant has no food, but food in sight, it will gather food.
             GatherFood();
         }
         else if (hasFood)
         {
             // If the ant has food, but no food in sight, it will return to the nest.
-            ReturnToNest();
+            ReturnToColony();
         }
+
     }
 
-    private void ReturnToNest()
+    private void ReturnToColony()
     {
-        throw new System.NotImplementedException();
+
     }
 
     private void Wander()
@@ -85,36 +90,37 @@ public class Ant : MonoBehaviour
     {
         hasFood = true;
         foodInSight.transform.SetParent(antHead);
+        foodInSight.GetComponent<Collider2D>().enabled = false;
     }
 
     #region Visualizations
     void OnDrawGizmos()
     {
         // Define the color for the field of view visualization
-    Gizmos.color = Color.blue;
+        Gizmos.color = Color.blue;
 
-    // Visualize the view angle using segments
-    int numSegments = 50; // Increase for smoother arc
-    Vector2 prevDir = DirFromAngle(-viewAngle / 2, false);
-    
-    for (int i = 0; i <= numSegments; i++)
-    {
-        float t = (float)i / (float)numSegments;
-        float currAngle = Mathf.Lerp(-viewAngle / 2, viewAngle / 2, t);
-        Vector2 currDir = DirFromAngle(currAngle, false);
-        
-        // Draw a segment of the field of view "slice"
-        Gizmos.DrawLine((Vector2)antHead.position + prevDir * viewRadius, (Vector2)antHead.position + currDir * viewRadius);
-        
-        prevDir = currDir;
-    }
+        // Visualize the view angle using segments
+        int numSegments = 50; // Increase for smoother arc
+        Vector2 prevDir = DirFromAngle(-viewAngle / 2, false);
 
-    // Draw the boundary lines of the slice
-    Vector2 viewAngleA = DirFromAngle(-viewAngle / 2, false);
-    Vector2 viewAngleB = DirFromAngle(viewAngle / 2, false);
+        for (int i = 0; i <= numSegments; i++)
+        {
+            float t = (float)i / (float)numSegments;
+            float currAngle = Mathf.Lerp(-viewAngle / 2, viewAngle / 2, t);
+            Vector2 currDir = DirFromAngle(currAngle, false);
 
-    Gizmos.DrawLine((Vector2)antHead.position, (Vector2)antHead.position + viewAngleA * viewRadius);
-    Gizmos.DrawLine((Vector2)antHead.position, (Vector2)antHead.position + viewAngleB * viewRadius);
+            // Draw a segment of the field of view "slice"
+            Gizmos.DrawLine((Vector2)antHead.position + prevDir * viewRadius, (Vector2)antHead.position + currDir * viewRadius);
+
+            prevDir = currDir;
+        }
+
+        // Draw the boundary lines of the slice
+        Vector2 viewAngleA = DirFromAngle(-viewAngle / 2, false);
+        Vector2 viewAngleB = DirFromAngle(viewAngle / 2, false);
+
+        Gizmos.DrawLine((Vector2)antHead.position, (Vector2)antHead.position + viewAngleA * viewRadius);
+        Gizmos.DrawLine((Vector2)antHead.position, (Vector2)antHead.position + viewAngleB * viewRadius);
     }
 
     Vector2 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
